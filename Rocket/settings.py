275a7a187e771c2eb067d52561a938539f881087
Alequita <71567007+Alequita@users.noreply.github.com>
2021-09-09 17:52:40 -0300

    Update settings.py

--- Rocket/settings.py
+++ Rocket/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#4dz3)oc-h+v-g1jcsh-k27=xcn4xu-tj2&%(vf0d4rx*j0(@c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['cloudsoft.herokuapp.com', '127.0.0.1']
 
